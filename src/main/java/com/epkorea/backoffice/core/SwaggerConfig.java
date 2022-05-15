cec8d109c9b6c5f62718df72e83305f2aac142af
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2022-05-15 15:16:58 +0900

    build: Swagger 추가 및 설정
    Swagger 의존성을 추가하고 설정 파일을 생성함.
    localhost:8080/swagger-ui/

---
+++ src/main/java/com/epkorea/backoffice/core/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.epkorea.backoffice.core;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("BackOffice")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.epkorea.backoffice.controller"))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("BackOffice")
+                .description("BackOffice REST API")
+                .version("1.0")
+                .build();
+    }
+}
