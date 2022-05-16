fd07ec24f3dd4ee3fb4cdc426bf3e58747eb6e3a
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2022-05-16 17:48:27 +0900

    feat: SessionInterceptor 세션 유효성 체크
    클라이언트 요청에 대한 세션의 유효성을 검사함

---
+++ src/main/java/com/epkorea/backoffice/interceptor/SessionInterceptor.java
@@ -0,0 +1,27 @@
+package com.epkorea.backoffice.interceptor;
+
+import com.epkorea.backoffice.entity.UserEntity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SessionInterceptor implements HandlerInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        UserEntity user = (UserEntity)session.getAttribute("user");
+        if (user == null) {
+            logger.info("Not Login User");
+            response.sendRedirect("/user/login");
+            return false;
+        }
+        logger.info("Login User: " + user.getUsername());
+        return true;
+    }
+}
