ab6ca0f8278adced6a1efd37a85cd73692b04e8d
rnjsrntkd95 <rnjsrntkd95@gmail.com>
2022-06-01 16:05:48 +0900

    refactor: DTO 클래스를 사용하여 데이터를 반환하도록 수정
    Projection.fields()를 사용하여 엔티티 반환 값이 DTO의 필드와 매핑되어 DTO 클래스로 반환되도록 수정함

--- src/main/java/com/epkorea/backoffice/repository/SocialCustomRepositoryImpl.java
+++ src/main/java/com/epkorea/backoffice/repository/SocialCustomRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.epkorea.backoffice.repository;
 
 import com.epkorea.backoffice.dto.SocialListPageDto;
-import com.epkorea.backoffice.entity.QSocialContribution;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -13,40 +12,44 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.epkorea.backoffice.entity.QSocialContribution.socialContribution;
+
 @Repository
 @RequiredArgsConstructor
 public class SocialCustomRepositoryImpl implements SocialCustomRepository {
 
     private final JPAQueryFactory queryFactory;
-    private final QSocialContribution s = QSocialContribution.socialContribution;
-
 
     @Override
     public Page<SocialListPageDto> findAllBySearchCondition(String condition, String kwd, Pageable pageable) {
-        List<SocialListPageDto> content =  queryFactory.select(Projections.bean(SocialListPageDto.class, s.title, s.showDate, s.isShow))
-                .from(s)
+        List<SocialListPageDto> content = queryFactory
+                .select(Projections.fields(
+                        SocialListPageDto.class,
+                        socialContribution.title,
+                        socialContribution.showDate,
+                        socialContribution.isShow))
+                .from(socialContribution)
                 .where(titleContains(condition, kwd), contentContains(condition, kwd))
-                .orderBy(s.showDate.desc(), s.sid.asc())
+                .orderBy(socialContribution.showDate.desc(), socialContribution.sid.asc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
         int totalSize = queryFactory
-                .selectFrom(s)
+                .selectFrom(socialContribution)
                 .where(titleContains(condition, kwd), contentContains(condition, kwd))
                 .fetch().size();
 
         return new PageImpl<>(content, pageable, totalSize);
 
 
-
     }
 
     private BooleanExpression titleContains(String condition, String kwd) {
-        return condition != null && condition.equals("title") ? s.title.contains(kwd) : null;
+        return condition != null && condition.equals("title") ? socialContribution.title.contains(kwd) : null;
     }
 
     private BooleanExpression contentContains(String condition, String kwd) {
-        return condition != null && condition.equals("content") ? s.content.contains(kwd) : null;
+        return condition != null && condition.equals("content") ? socialContribution.content.contains(kwd) : null;
     }
 }
